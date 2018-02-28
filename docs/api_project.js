725ce574bcea65a07a7675942d86c27f8b649164
Florian Schaller <florian.schaller@heig-vd.ch>
2018-02-28 12:02:18 +0100

    Write APIDOC for user endpoints

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "Webserv Project",  "version": "1.0.0",  "description": "Creation of an API for a school project",  "title": "Webserv Project",  "url": "https://webserv-flola.herokuapp.com",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-02-28T10:10:54.349Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({
+  "name": "Webserv Project",
+  "version": "1.0.0",
+  "description": "Creation of an API for a school project",
+  "title": "Webserv Project",
+  "url": "https://webserv-flola.herokuapp.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-28T11:00:13.518Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
